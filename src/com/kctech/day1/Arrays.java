a65bac9b5df5958313242ee4766345a5f869a315
Martin <martin_sr@kctechservices.com>
2021-02-16 17:18:32 +0530

    memory

---
+++ src/com/kctech/day1/Arrays.java
@@ -0,0 +1,21 @@
+package com.kctech.day1;
+
+public class Arrays {
+	public static void main(String args[])
+	{
+		int a[]=new int[5];
+	    a[0]=5;
+	    a[1]=10;
+	    a[2]=15;
+	    a[3]=20;
+	    for(int i=0; i<=a.length; i++)
+	    {
+	    	System.out.println(a[i]);
+	    }
+	    
+			
+	  }
+		
+	}
+
+
